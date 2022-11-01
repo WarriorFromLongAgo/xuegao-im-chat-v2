79294ae18ba0d503013a1562ba1dc78aac549450
lzcndm <lzcndm@qq.com>
2022-11-01 09:24:47 +0800

    Update LoginAndRegister.vue
    表单内的button点击事件同时触发了表单的提交事件

--- xuegao-vue-wechat-demo-v2/src/views/system/login/LoginAndRegister.vue
+++ xuegao-vue-wechat-demo-v2/src/views/system/login/LoginAndRegister.vue
@@ -14,7 +14,7 @@
             placeholder="确认密码"
           />
           <!--          <router-view :to="{ name: 'imHome' }">-->
-          <button class="register-btn" @click="systemRegister">注册</button>
+          <button class="register-btn" @click.prevent="systemRegister">注册</button>
           <!--          </router-view>-->
           <!--          <div class="register-tips">tips：昵称可以在登录后随便修改</div>-->
         </form>
@@ -34,7 +34,7 @@
           <div class="remember-me">
             <input type="checkbox" /><span>记住我</span>
           </div>
-          <button class="login-btn" @click="systemLogin">登录</button>
+          <button class="login-btn" @click.prevent="systemLogin">登录</button>
           <!--        <div class="login-other-operate">-->
           <!--          <a href="#">忘记密码</a> |-->
           <!--          <a href="#">注册新账号</a>-->
